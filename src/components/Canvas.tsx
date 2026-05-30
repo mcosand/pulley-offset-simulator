9b74a46bae896cab6d4c8182ed586f8935d85a20
Matt Cosand <matt@cosand.net>
2026-05-30 00:44:53 -0700

    Better mobile touch

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -36,18 +36,18 @@ export const Canvas = observer(({ store }: { store: Store }) => {
   }
 
   return (
-    <svg width="800" height="600" id="svg-canvas" style={{ border: 'solid 1px #444' }}
-    onMouseMove={move} onMouseUp={up} viewBox="0 0 100 100">
+    <svg width="800" height="600" id="svg-canvas" style={{ border: 'solid 1px #444', maxWidth:'100%', backgroundColor:'#fff' }}
+    onPointerMove={move} onPointerUp={up} viewBox="0 0 100 100">
       {store.showGuides && <circle id="radius" cx={store.anchors.red.x} cy={store.anchors.red.y} r={store.redLength} style={{ fill: 'transparent', stroke: '#0003', strokeWidth: .2 }} />}
       {store.showGuides && store.pulley?.deflection && store.bisectEnd && <line id="bisect" x1={store.pulley.x} y1={store.pulley.y} x2={store.bisectEnd.x} y2={store.bisectEnd.y} style={{ stroke: '#0004', strokeWidth: .2, strokeDasharray: '1 2' }} />}
 
-      <line id="black1" x1={store.anchors.black.x} y1={store.anchors.black.y} x2={store.blackMiddle.x} y2={store.blackMiddle.y} style={{ stroke: '#444', strokeWidth: .2 }} />
-      <line id="black2" x1={store.blackMiddle.x} y1={store.blackMiddle.y} x2={store.blackEnd.x} y2={store.blackEnd.y} style={{ stroke: '#444', strokeWidth: .2 }} />
-      <line id="redrope" x1={store.anchors.red.x} y1={store.anchors.red.y} x2={store.pulleyEx.x} y2={store.pulleyEx.y} style={{ stroke: '#f00', strokeWidth: .2 }} />
+      <line id="black1" x1={store.anchors.black.x} y1={store.anchors.black.y} x2={store.blackMiddle.x} y2={store.blackMiddle.y} style={{ stroke: '#444', strokeWidth: .4 }} />
+      <line id="black2" x1={store.blackMiddle.x} y1={store.blackMiddle.y} x2={store.blackEnd.x} y2={store.blackEnd.y} style={{ stroke: '#444', strokeWidth: .4 }} />
+      <line id="redrope" x1={store.anchors.red.x} y1={store.anchors.red.y} x2={store.pulleyEx.x} y2={store.pulleyEx.y} style={{ stroke: '#f00', strokeWidth: .3 }} />
 
       <circle id="pulley" cx={store.pulleyEx.x} cy={store.pulleyEx.y} r="2" style={{ fill: '#0f08' }} />
-      <circle id="red" onMouseDown={onDown} cx={store.anchors.red.x} cy={store.anchors.red.y} r="2" style={{ fill: '#800', pointerEvents: 'auto' }} />
-      <circle id="black" onMouseDown={onDown} cx={store.anchors.black.x} cy={store.anchors.black.y} r="2" style={{ fill: '#444', pointerEvents: 'auto' }} />
+      <circle id="red" className="drag" onPointerDown={onDown} cx={store.anchors.red.x} cy={store.anchors.red.y} r="2" style={{ fill: '#800' }} />
+      <circle id="black" className="drag" onPointerDown={onDown} cx={store.anchors.black.x} cy={store.anchors.black.y} r="2" style={{ fill: '#444' }} />
       {store.searchPoints.map(p => (<text key={p.l} x={p.x} y={p.y} style={{fontSize: '3px'}}>{p.l}</text>))}
     </svg>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    allowedHosts: true,
+  },
   plugins: [    react({
       // Enable JSX Decorators
       // NOTE: we need this for MobX
